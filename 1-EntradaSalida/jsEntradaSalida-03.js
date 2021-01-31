99b6aeaead27a6a3a0e8cb9b70e5c418ce9174f2
lucianaturco <lucianaturco1@gmail.com>
2021-01-31 12:06:50 -0300

    nuevo commit

--- 1-EntradaSalida/jsEntradaSalida-03.js
+++ 1-EntradaSalida/jsEntradaSalida-03.js
@@ -3,10 +3,15 @@ Debemos lograr tomar un dato por 'ID'
 y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	var nombreIngresado;
-	nombreIngresado=txtIdNombre.value;
-	alert("ok");
+	let nombre;
 
+	nombre = document.getElementById("txtIdNombre").value; 
+
+	alert(nombre);
+
+/*	document.getElementById("txtIdNombre").value = "" 
+	esto es para que la caja de texto quede vacía para el proximo ingreso */
+	
 }
 
 
